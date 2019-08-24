6b80a0d4525d9c6f7444b033e9ffc65639d03ee4
Isaac <isaac.rebolledoleal@gmail.com>
2019-08-24 17:46:18 -0400

    Number Page with Landscape and Portrait view optimized

--- src/Hymnal.UI/Pages/Tabs/NumberPage.xaml.cs
+++ src/Hymnal.UI/Pages/Tabs/NumberPage.xaml.cs
@@ -2,6 +2,7 @@ using System;
 using Hymnal.Core.ViewModels;
 using MvvmCross.Forms.Presenters.Attributes;
 using MvvmCross.Forms.Views;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Hymnal.UI.Pages
@@ -14,11 +15,12 @@ namespace Hymnal.UI.Pages
         {
             InitializeComponent();
 
-            //SizeChanged += (s, args) =>
-            //{
-            //    var visualState = Width > Height ? "Landscape" : "Portrait";
-            //    VisualStateManager.GoToState(MainStack, visualState);
-            //};
+            SizeChanged += (s, args) =>
+            {
+                var visualState = Width > Height ? "Landscape" : "Portrait";
+                VisualStateManager.GoToState(MainStack, visualState);
+                VisualStateManager.GoToState(ContainerStack, visualState);
+            };
         }
 
         private void OpenButton_Clicked(object sender, EventArgs e)
