246ea5676f7f27c25121b6ee159b17602ef657ec
Isaac <isaac.rebolledoleal@gmail.com>
2019-12-10 16:50:31 -0300

    Dark Mode implemented
    - iOS Dark mode implemented in X.F.

--- src/Hymnal.UI/Resources/DarkTheme.xaml.cs
+++ src/Hymnal.UI/Resources/DarkTheme.xaml.cs
@@ -4,9 +4,9 @@ using Xamarin.Forms.Xaml;
 namespace Hymnal.UI.Resources
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Colors : ResourceDictionary
+    public partial class DarkTheme : ResourceDictionary
     {
-        public Colors()
+        public DarkTheme()
         {
             InitializeComponent();
         }
---
+++ src/Hymnal.UI/Resources/WhiteTheme.xaml.cs
@@ -0,0 +1,14 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Hymnal.UI.Resources
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WhiteTheme : ResourceDictionary
+    {
+        public WhiteTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}
