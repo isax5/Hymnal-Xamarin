6c1f8885e2e2d1839aebc7c89789da36c6d065c4
Isax95 <isaac.rebolledo@licklider.cl>
2020-09-05 07:55:08 -0400

    Issue #90 Uso de PageSheet en records

--- src/Hymnal.XF.UI/Pages/Records/RecordsPage.xaml.cs
+++ src/Hymnal.XF.UI/Pages/Records/RecordsPage.xaml.cs
@@ -1,17 +1,38 @@
 using Hymnal.Core.ViewModels;
 using MvvmCross.Forms.Presenters.Attributes;
 using MvvmCross.Forms.Views;
+using MvvmCross.Presenters;
+using MvvmCross.Presenters.Attributes;
+using MvvmCross.ViewModels;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Hymnal.XF.UI.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    [MvxContentPagePresentation(WrapInNavigationPage = true, NoHistory = false)]
-    public partial class RecordsPage : MvxContentPage<RecordsViewModel>
+    public partial class RecordsPage : MvxContentPage<RecordsViewModel>, IMvxOverridePresentationAttribute
     {
         public RecordsPage()
         {
             InitializeComponent();
+        }
+
+        public MvxBasePresentationAttribute PresentationAttribute(MvxViewModelRequest request)
+        {
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    return new MvxModalPresentationAttribute
+                    {
+                        WrapInNavigationPage = false
+                    };
+                default:
+                    return new MvxContentPagePresentationAttribute
+                    {
+                        WrapInNavigationPage = true,
+                        NoHistory = false
+                    };
+            }
         }
     }
 }
