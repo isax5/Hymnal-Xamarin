0a6e7f3828744c209847416ddd818582a2342841
ISAX-PC\isaac <isaac.rebolledoleal@gmail.com>
2022-11-20 09:19:17 -0300

    Llevar eventos de Page a ViewModel

--- Hymnal/ViewModels/Base/BaseViewModel.cs
+++ Hymnal/ViewModels/Base/BaseViewModel.cs
@@ -9,4 +9,44 @@ public abstract partial class BaseViewModel : ObservableObject
     private bool busy;
 
     public bool NotBusy => Busy;
+
+
+    #region Life Cycle
+    public virtual void Initialize(NavigatedToEventArgs args)
+    { }
+
+    public virtual Task InitializeAsync(NavigatedToEventArgs args)
+    {
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// When navigate from this VM
+    /// </summary>
+    /// <param name="args"></param>
+    public virtual void OnNavigatedFrom(NavigatedFromEventArgs args)
+    { }
+
+    /// <summary>
+    /// When navigate to this VM
+    /// </summary>
+    /// <param name="args"></param>
+    public virtual void OnNavigatedTo(NavigatedToEventArgs args)
+    { }
+
+    public virtual void OnNavigatingFrom(NavigatingFromEventArgs args)
+    { }
+
+    /// <summary>
+    /// On Appearing
+    /// </summary>
+    public virtual void OnAppearing()
+    { }
+
+    /// <summary>
+    /// On Disappearing
+    /// </summary>
+    public virtual void OnDisappearing()
+    { }
+    #endregion
 }
--- Hymnal/Views/Base/BaseContentPage.cs
+++ Hymnal/Views/Base/BaseContentPage.cs
@@ -1,3 +1,5 @@
+using Hymnal.ViewModels;
+
 namespace Hymnal.Views;
 
 public class BaseContentPage<TViewModel> : BaseContentPage where TViewModel : class
@@ -8,8 +10,59 @@ public class BaseContentPage<TViewModel> : BaseContentPage where TViewModel : cl
         set => BindingContext = value;
     }
 
+
     public BaseContentPage(TViewModel viewModel)
     {
         ViewModel = viewModel;
+    }
+
+    protected override void OnNavigatedFrom(NavigatedFromEventArgs args)
+    {
+        if (ViewModel is BaseViewModel viewModel)
+            viewModel.OnNavigatedFrom(args);
+
+        base.OnNavigatedFrom(args);
+    }
+
+    private bool pageLoaded;
+    protected override void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        if (ViewModel is BaseViewModel viewModel)
+        {
+            if (!pageLoaded)
+            {
+                viewModel.Initialize(args);
+                viewModel.InitializeAsync(args).ConfigureAwait(true);
+            }
+
+            viewModel.OnNavigatedTo(args);
+        }
+
+        pageLoaded = true;
+        base.OnNavigatedTo(args);
+    }
+
+    protected override void OnNavigatingFrom(NavigatingFromEventArgs args)
+    {
+        if (ViewModel is BaseViewModel viewModel)
+            viewModel.OnNavigatingFrom(args);
+
+        base.OnNavigatingFrom(args);
+    }
+
+    protected override void OnAppearing()
+    {
+        if (ViewModel is BaseViewModel viewModel)
+            viewModel.OnAppearing();
+
+        base.OnAppearing();
+    }
+
+    protected override void OnDisappearing()
+    {
+        if (ViewModel is BaseViewModel viewModel)
+            viewModel.OnDisappearing();
+
+        base.OnDisappearing();
     }
 }
