065e34665968d8fe6aeb6315fc6d3301a6309a7c
ISAX-PC\isaac <isaac.rebolledoleal@gmail.com>
2023-02-15 08:05:27 -0300

    Avanzando en versiones de paquetes, diseño, etc.

--- Hymnal/ViewModels/Tabs/ThematicIndexViewModel.cs
+++ Hymnal/ViewModels/Tabs/ThematicIndexViewModel.cs
@@ -2,4 +2,27 @@ namespace Hymnal.ViewModels;
 
 public sealed partial class ThematicIndexViewModel : BaseViewModel
 {
+    private readonly HymnsService hymnsService;
+
+    #region Properties
+    public ObservableRangeCollection<Thematic> Thematics { get; } = new();
+    #endregion
+
+
+    public ThematicIndexViewModel(HymnsService hymnsService)
+    {
+        this.hymnsService = hymnsService;
+    }
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        hymnsService.GetThematicListAsync(InfoConstants.HymnsLanguages.First())
+            .ToObservable()
+            .Subscribe(result => MainThread.BeginInvokeOnMainThread(delegate
+            {
+                Thematics.ReplaceRange(result);
+            }), error => error.Report());
+    }
 }
